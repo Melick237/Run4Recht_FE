99047f201011c9563ed49ec0f0881a6c09397326
jackAinsworth <ainswort@th-brandenburg.de>
2024-06-17 14:23:10 +0200

    init commit

---
+++ Run4Recht/capacitor.config.ts
@@ -0,0 +1,16 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Run4Recht',
+  webDir: 'www',
+  plugins: {
+    LocalNotifications: {
+      smallIcon: 'ic_notification',
+      iconColor: '#488AFF',
+      sound: 'beep.wav',
+    }
+  }
+};
+
+export default config;
